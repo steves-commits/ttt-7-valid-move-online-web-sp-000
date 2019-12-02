d2fd63694de52e653f017554d2c96f6b97d96b26
Steven Topazio <sat@northpointprinting.com>
2019-12-02 16:08:11 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,9 @@
 # code your #valid_move? method here
-
+def valid_move?
+  if 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def #position_taken? (board, index)
+def position_taken? (board, index)
 if board[index] == " " || board[index] == "" || board[index] == NIL
   returns FALSE
 else
